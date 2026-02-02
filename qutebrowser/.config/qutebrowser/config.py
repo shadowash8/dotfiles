78aba3ebd2947d4f02aa2481a305c9392add12dd
A S H <81229115+shadowash8@users.noreply.github.com>
2026-02-02 21:24:16 +0530

    qutebrowser: dark mode image policy

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -53,7 +53,7 @@ for i in range(1, 10):
 # dark mode setup
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
-c.colors.webpage.darkmode.policy.images = 'smart'
+c.colors.webpage.darkmode.policy.images = 'never'
 config.set('colors.webpage.darkmode.enabled', False, 'file://*')
 
 # styles, cosmetics
