f96970a8da7cf4c2cc79359ab87823425db64120
A S H <81229115+shadowash8@users.noreply.github.com>
2026-02-10 18:58:39 +0530

    nvim: update colorschemes and lualine

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,46 +1,5 @@
 return {
     {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("tokyonight").setup({
-                style = "night",
-                sidebars = { "qf", "help", "NvimTree" },
-            })
-            -- vim.cmd([[colorscheme tokyonight]])
-        end,
-    },
-    {
-        "drewxs/ash.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            -- vim.cmd([[colorscheme ash]])
-        end,
-    },
-    {
-        "craftzdog/solarized-osaka.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            transparent = true,
-            styles = {
-                sidebars = "transparent",
-                floats = "transparent",
-            },
-        },
-    },
-    {
-        "oskarnurm/koda.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("koda").setup({ transparent = true })
-            -- vim.cmd("colorscheme koda")
-        end,
-    },
-    {
         'uZer/pywal16.nvim',
         config = function()
             vim.cmd.colorscheme("pywal16")
