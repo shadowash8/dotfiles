b6196b83946d693d295aab7f92249bd82c983373
A S H <shadowash8@protonmail.com>
2026-03-05 08:26:08 +0530

    feat: fix neovim lsp and formatter

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,53 +1,67 @@
 local utils = require("utils")
 local blink_cmp = require("blink.cmp")
 
+-- This bridges Mason installs → vim.lsp.config automatically
+require("mason").setup()
+require("mason-lspconfig").setup({
+	-- These will be auto-installed if missing
+	ensure_installed = { "lua_ls", "ts_ls" },
+	automatic_installation = true,
+})
+
 local lsp_configs = {
-    lua_ls = {
-        cmd = { "lua-language-server" },
-        filetypes = { "lua" },
-        root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
-        settings = {
-            Lua = {
-                runtime = { version = "LuaJIT" },
-                diagnostics = { globals = { "vim" } },
-                workspace = { library = { vim.env.VIMRUNTIME } },
-            },
-        },
-    },
-    ts_ls = {
-        cmd = { "typescript-language-server", "--stdio" },
-        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-        root_markers = { "package.json", "tsconfig.json", ".git" },
-    },
+	lua_ls = {
+		filetypes = { "lua" },
+		root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
+		settings = {
+			Lua = {
+				runtime = { version = "LuaJIT" },
+				diagnostics = { globals = { "vim" } },
+				workspace = { library = { vim.env.VIMRUNTIME } },
+			},
+		},
+	},
+	ts_ls = {
+		filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+		root_markers = { "package.json", "tsconfig.json", ".git" },
+	},
 }
 
--- Setup each language server
 for server, config in pairs(lsp_configs) do
-    config.capabilities = blink_cmp.get_lsp_capabilities(config.capabilities)
-    vim.lsp.config(server, config)
-    vim.lsp.enable(server)
+	config.capabilities = blink_cmp.get_lsp_capabilities(config.capabilities)
+	vim.lsp.config(server, config)
+	vim.lsp.enable(server)
 end
 
--- Stup mason
-require("mason").setup()
+-- Formatters via conform.nvim (handles prettier, stylua, etc. from Mason)
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescript = { "prettier" },
+		typescriptreact = { "prettier" },
+	},
+})
 
+-- Make <leader>lf use conform instead of LSP format
 -- Diagnostics
 vim.diagnostic.config({
-    virtual_text = true,
-    signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = "",
-            [vim.diagnostic.severity.WARN]  = "",
-            [vim.diagnostic.severity.INFO]  = "󰠠",
-            [vim.diagnostic.severity.HINT]  = "",
-        },
-        numhl = {
-            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-            [vim.diagnostic.severity.WARN]  = "WarningMsg",
-            [vim.diagnostic.severity.INFO]  = "DiagnosticInfo",
-            [vim.diagnostic.severity.HINT]  = "DiagnosticHint",
-        },
-    },
+	virtual_text = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.INFO] = "󰠠",
+			[vim.diagnostic.severity.HINT] = "",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+			[vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+			[vim.diagnostic.severity.HINT] = "DiagnosticHint",
+		},
+	},
 })
 
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { underline = true, undercurl = true, sp = "Red" })
@@ -57,7 +71,9 @@ vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { underline = true, undercurl 
 
 -- LSP keymaps
 utils.nmap_leader("la", vim.lsp.buf.code_action, "Code Action")
-utils.nmap_leader("lf", function() vim.lsp.buf.format({ async = true }) end, "Format")
+utils.nmap_leader("lf", function()
+	require("conform").format({ async = true, lsp_fallback = true })
+end, "Format")
 utils.nmap_leader("lr", vim.lsp.buf.rename, "Rename")
 
 utils.nmap_leader("ld", vim.lsp.buf.definition, "Go to Definition")
--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,14 +1,39 @@
 return {
-    -- Mason for managing LSP servers
-    {
-        'williamboman/mason.nvim',
-        lazy = false,
-        opts = {
-            auto_install = true, 
-        },
-        config = true,
-        keys = {
-            { "<leader>lm", "<cmd>Mason<cr>", desc = "Mason" },
-        }
-    },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+		config = true,
+		keys = {
+			{ "<leader>lm", "<cmd>Mason<cr>", desc = "Mason" },
+		},
+	},
+
+	-- Bridges Mason installs → vim.lsp automatically
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		dependencies = { "williamboman/mason.nvim" },
+		opts = {
+			ensure_installed = { "lua_ls", "ts_ls" },
+			automatic_installation = true,
+		},
+	},
+
+	-- Formatter runner (prettier, stylua, etc.)
+	{
+		"stevearc/conform.nvim",
+		lazy = false,
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+			},
+		},
+	},
 }
