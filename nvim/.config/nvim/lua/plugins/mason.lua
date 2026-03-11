575497293c79ec528abced867b1c020903df14a4
A S H <shadowash8@protonmail.com>
2026-03-11 09:42:27 +0530

    major changes in nvim config

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,30 +1,10 @@
 return {
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {},
-        dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
-        },
-    },
-
-	-- Formatter runner (prettier, stylua, etc.)
 	{
-		"stevearc/conform.nvim",
-		lazy = false,
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-			},
-			formatters = {
-				prettier = {
-					prepend_args = { "--tab-width", "4" },
-				},
-			},
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
 		},
 	},
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,23 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = "BufReadPre",
-    },
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		auto_install = true,
+		indent = { enable = true },
+		ensure_installed = {
+			"vim",
+			"lua",
+			"vimdoc",
+			"html",
+			"css",
+			"javascript",
+			"typescript",
+			"tsx",
+			"markdown",
+			"markdown_inline",
+			"json",
+			"scss",
+			"yaml",
+			"php",
+		},
+	},
 }
