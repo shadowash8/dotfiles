c3c14d50e7c41110d367a717cf75da5b3bdc5112
A S H <shadowash8@protonmail.com>
2026-04-11 20:22:49 +0530

    misc script updates

--- scripts/.config/scripts/autostart.sh
+++ scripts/.config/scripts/autostart.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-swayidle &
 dunst &
+swayidle -w \
+      timeout 300 "systemctl suspend" \
+      before-sleep "gtklock" &
+kdeconnectd &
 wlsunset -o eDP-1 -l 19 -L 74 -g 0.8 &
 
 # Clipboard stuff
