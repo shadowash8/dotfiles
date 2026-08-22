7e944071b2850094b091b23d48c7ef3cd7521613
A S H <shadowash8@protonmail.com>
2026-08-22 23:52:42 +0530

    feat: add qutebrowser ashwal colors

---
+++ ashwal/.config/ashwal/templates/colors-qutebrowser.py
@@ -0,0 +1,89 @@
+# --- Statusbar ---
+c.colors.statusbar.normal.bg = "{color0.hex}"
+c.colors.statusbar.command.bg = "{color0.hex}"
+c.colors.statusbar.command.fg = "{color15.hex}"
+c.colors.statusbar.normal.fg = "{color2.hex}"
+c.colors.statusbar.insert.fg = "{color0.hex}"
+c.colors.statusbar.insert.bg = "{color4.hex}"
+c.colors.statusbar.passthrough.fg = "{color0.hex}"
+c.colors.statusbar.passthrough.bg = "{color6.hex}"
+c.colors.statusbar.url.fg = "{color15.hex}"
+c.colors.statusbar.url.error.fg = "{color1.hex}"
+c.colors.statusbar.url.hover.fg = "{color6.hex}"
+c.colors.statusbar.url.success.http.fg = "{color2.hex}"
+c.colors.statusbar.url.success.https.fg = "{color4.hex}"
+
+# --- Tabs ---
+c.colors.tabs.bar.bg = "{color0.hex}"
+c.colors.tabs.even.bg = "{color0.hex}"
+c.colors.tabs.odd.bg = "{color0.hex}"
+c.colors.tabs.even.fg = "{color15.hex}"
+c.colors.tabs.odd.fg = "{color15.hex}" 
+c.colors.tabs.selected.even.bg = "{color1.hex}"
+c.colors.tabs.selected.odd.bg = "{color1.hex}"
+c.colors.tabs.selected.even.fg = "{color6.hex}"
+c.colors.tabs.selected.odd.fg = "{color6.hex}"
+c.colors.hints.bg = "{color0.hex}"
+c.colors.hints.fg = "{color15.hex}"
+c.tabs.show = "multiple"
+
+# --- Completion ---
+c.colors.completion.odd.bg = "{color0.hex}"
+c.colors.completion.even.bg = "{color0.hex}"
+c.colors.completion.fg = "{color15.hex}"
+c.colors.completion.category.bg = "{color0.hex}"
+c.colors.completion.category.fg = "{color4.hex}"
+c.colors.completion.category.border.top = "{color0.hex}"
+c.colors.completion.category.border.bottom = "{color0.hex}"
+c.colors.completion.item.selected.bg = "{color8.hex}"
+c.colors.completion.item.selected.fg = "{color15.hex}"
+c.colors.completion.item.selected.border.top = "{color8.hex}"
+c.colors.completion.item.selected.border.bottom = "{color8.hex}"
+c.colors.completion.item.selected.match.fg = "{color6.hex}"
+c.colors.completion.match.fg = "{color6.hex}"
+c.colors.completion.scrollbar.fg = "{color15.hex}"
+c.colors.completion.scrollbar.bg = "{color0.hex}"
+
+# --- Context Menu ---
+c.colors.contextmenu.disabled.bg = "{color0.hex}"
+c.colors.contextmenu.disabled.fg = "{color7.hex}"
+c.colors.contextmenu.menu.bg = "{color0.hex}"
+c.colors.contextmenu.menu.fg = "{color15.hex}"
+c.colors.contextmenu.selected.bg = "{color8.hex}"
+c.colors.contextmenu.selected.fg = "{color15.hex}"
+
+# --- Messages ---
+c.colors.messages.info.bg = "{color0.hex}"
+c.colors.messages.info.fg = "{color6.hex}"
+c.colors.messages.error.bg = "{color0.hex}"
+c.colors.messages.error.fg = "{color1.hex}"
+c.colors.messages.warning.fg = "{color3.hex}"
+c.colors.messages.warning.bg = "{color0.hex}"
+
+# --- Downloads ---
+c.colors.downloads.error.bg = "{color0.hex}"
+c.colors.downloads.error.fg = "{color1.hex}"
+c.colors.downloads.bar.bg = "{color0.hex}"
+c.colors.downloads.start.bg = "{color2.hex}"
+c.colors.downloads.start.fg = "{color15.hex}"
+c.colors.downloads.stop.bg = "{color8.hex}"
+c.colors.downloads.stop.fg = "{color15.hex}"
+
+# --- Hints ---
+c.colors.hints.bg = "{color0.hex}"
+c.colors.hints.fg = "{color15.hex}"
+c.hints.border = "1px solid {color15.hex}"
+
+# --- Tooltip ---
+c.colors.tooltip.bg = "{color0.hex}"
+c.colors.tooltip.fg = "{color15.hex}"
+
+# --- Prompts ---
+c.colors.prompts.bg = "{color0.hex}"
+c.colors.prompts.fg = "{color15.hex}"
+c.colors.prompts.border = "1px solid {color15.hex}"
+c.colors.prompts.selected.bg = "{color8.hex}"
+c.colors.prompts.selected.fg = "{color15.hex}"
+
+# --- Webpage ---
+c.colors.webpage.bg = "{color0.hex}"
